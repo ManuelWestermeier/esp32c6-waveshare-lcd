de4f86b8e9bee9916e071d483a9cc6944449cc1c
ManuelWestermeier <westermeier111@gmail.com>
2025-05-15 17:48:47 +0200

    other folderstructure

--- esp32c6-waveshare-lcd.ino
+++ esp32c6-waveshare-lcd.ino
@@ -1,12 +1,13 @@
-#include "colors.hpp"
-#include "metadata.hpp"
-#include "input.hpp"
-#include "init.hpp"
-#include "init-screen.hpp"
+#include "src/colors.hpp"
+#include "src/metadata.hpp"
+#include "src/input.hpp"
+#include "src/init.hpp"
+#include "src/init-screen.hpp"
 
 #define USE_INIT_SCREEN true
 
-void setup() {
+void setup()
+{
   init();
   Input::start();
 #if USE_INIT_SCREEN
@@ -14,5 +15,6 @@ void setup() {
 #endif
 }
 
-void loop() {
+void loop()
+{
 }
